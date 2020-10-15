37c9e2ecae7c9fd990ec9f81e719e207b1e88f86
Automatycs <71386381+Automatycs@users.noreply.github.com>
2020-10-15 17:52:05 +0200

    [FIX] Correction de l'envoie de message
    Appuyer sur "Entrée" envoie maintenant un message.
    Envoyer un message vide maintenant la zone d'input.

--- public/index.js
+++ public/index.js
@@ -1,22 +1,43 @@
 function ClearInput() {
-  document.getElementById("send").value = null;
+  document.getElementById("m").value = null;
 }
 (() => {
   const socket = io();
-  $('#send').click(() => {
-    let msg = $('#m').val();
-    socket.emit('chat message', msg);
-    $('m').value = null;
-  });
+
+  
+  // Détecte les évènements d'envoie de messages:
+  // - appuie sur le bouton "envoyer"
+  // - appuie sur la touche entrée du clavier
+  // Envoie le message
+  // Vide le contenu de l'input 'm'
+
+  $('form').submit(function(e) {
+    e.preventDefault();
+    socket.emit('chat message', $('#m').val());
+    $('#m').val('');
+  })
+
+
+  // Créer un nouvel élément 'li'
+  // Met le contenu du message 'msg' dans le nouvel élément
+  // Ajoute le nouvel élément à la liste de messages
 
   socket.on('chat message', function(msg) {
     let elem = document.createElement('li');
     elem.innerHTML = msg;
     $('#messages').append(elem);
   });
+
+
+  // Envoie une notification de connection sur le log
+
   socket.on('connect', () => {
     console.log(socket.connected);
   });
+
+
+  // Envoie une notification de déconnection sur le log
+
   socket.on('disconnect', () => {
     console.log(socket.connected);
   });
